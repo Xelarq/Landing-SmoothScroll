7c6311b37b8305deeb3b3d3c732456e61ebdf250
Alexander Reva <revaalex123@gmail.com>
2023-03-27 16:54:47 +0300

    fix
    fix

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 const burgerBtn = document.getElementById("burger-btn");
 const closeBtn = document.getElementById("close-btn");
-const mobileMenu = document.querySelector('.mobile-nav-menu-wrapper');
+const mobileMenu = document.querySelector('.mobile-nav');
 
 burgerBtn.addEventListener('click', (event) => {mobileMenu.classList.add('show')})
 closeBtn.addEventListener('click', (event) => {mobileMenu.classList.remove('show')})
